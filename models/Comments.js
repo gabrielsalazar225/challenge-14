7bb06d92b291f83136918649a32c4bd3ada1aa74
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-08-04 14:58:44 -0400

    routes

---
+++ models/Comments.js
@@ -0,0 +1,26 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {};
+
+Comment.init ({
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},
+{
+    sequelize,
+    freezeTableName: true,
+    timestamps: true,
+    underscored: true,
+    modelName: 'comment'
+
+});
+module.exports = Comment;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+const PORT = process.env.Port || 3001;
+const session = require('express-session');
+
+const sequelize = require('./config/connection');
+
+// const model = require('./models');
+
+// const routes = require('./controlles');
+
+app.use(session({
+    secret: 'your_secret_key_here',
+    resave: false,
+    saveUninitalized: true,
+    cookie: { secure: true }
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(routes);
+
+sequelize.sync({force: true}).then(()=>
+app.listen(PORT, ()=> {
+    console.log(`Server Running on port ${PORT}`)
+}));
